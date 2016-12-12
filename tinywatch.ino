bfbbff2152b203977cb10cb92eecd4e51aba7cb2
Christopher Bolgiano <cbolgiano625@gmail.com>
2016-12-12 18:43:40 -0500

    TW-8: Connected to phone using BLE.

--- tinywatch.ino
+++ tinywatch.ino
@@ -1,34 +1,64 @@
 #include <SPI.h>
 #include <TimeLib.h> //include the Arduino Time library
+#include <BLEPeripheral.h>
 #include "lib_RenderBuffer.h"
 #include "lib_StringBuffer.h"
 
 TinyScreen display = TinyScreen(TinyScreenPlus);
 RenderBuffer<uint8_t,20> buffer;
 
-#define BLE_DEBUG false
-uint8_t ble_rx_buffer[21];
-uint8_t ble_rx_buffer_len = 0;
-uint8_t ble_connection_state = false;
-uint8_t ble_connection_displayed_state = true;
+#define BLE_REQ 10
+#define BLE_RDY 2
+#define BLE_RST 9
+
+BLEPeripheral blePeripheral = BLEPeripheral(BLE_REQ, BLE_RDY, BLE_RST);
+
+BLEService service = BLEService("CCC0");
+BLECharCharacteristic characteristic = BLECharCharacteristic("CCC1", BLERead | BLENotify);
+BLEDescriptor desc = BLEDescriptor("2901", "value");
 
 void setup() {
-    display.begin();
-    display.setFlip(true);
-    display.setBrightness(8);
-    display.setBitDepth(buffer.is16bit());
+  BLESetup();
+  display.begin();
+  display.setFlip(true);
+  display.setBrightness(8);
+  display.setBitDepth(buffer.is16bit());
 
-    setTime(13,19,55,6,3,2016); //values in the order hr,min,sec,day,month,year
+  setTime(13,19,55,6,3,2016); //values in the order hr,min,sec,day,month,year
 }
 
 void loop(void) {
-  unsigned int n = millis() >> 4;
+  blePeripheral.poll();
+}
 
-  renderTime(n);
-  renderDate(n);
+void BLESetup(){
+  blePeripheral.setLocalName("tinywatch");
 
-  buffer.flush(display);
-  stringBuffer.reset();
+  blePeripheral.setAdvertisedServiceUuid(service.uuid());
+  blePeripheral.addAttribute(service);
+  blePeripheral.addAttribute(characteristic);
+  blePeripheral.addAttribute(desc);
+  
+  blePeripheral.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+  blePeripheral.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+ 
+  blePeripheral.begin();
+}
+
+void blePeripheralConnectHandler(BLECentral& central) {
+  if (central){
+    buffer.drawText(stringBuffer.start().put("connected...").get(),15,8,buffer.rgb(255,0,0), &virtualDJ_5ptFontInfo);    
+    buffer.flush(display);
+    stringBuffer.reset();
+  }
+}
+
+void blePeripheralDisconnectHandler(BLECentral& central) {
+  if (central){
+    buffer.drawText(stringBuffer.start().put("disconnected...").get(),15,8,buffer.rgb(255,0,0), &virtualDJ_5ptFontInfo);
+    buffer.flush(display);
+    stringBuffer.reset();
+  }
 }
 
 void renderTime(unsigned int n){
@@ -38,6 +68,7 @@ void renderTime(unsigned int n){
 void renderDate(unsigned int n){
   buffer.drawText(stringBuffer.start().putDec(month()).put("-").putDec(day()).put("-").putDec(year()).get(),15,16,buffer.rgb(255,0,0), &virtualDJ_5ptFontInfo);
 }
+
 
 //TODO: Method to render background.
 
