0ff3abc2afd16325e5761c9bdc6cabb29f6311fb
cbolgiano <cbolgiano625@gmail.com>
2017-12-31 13:03:58 -0500

    TW-86: Effort to follow Arduino style guidelines for ino file.

--- src/tinywatch/tinywatch.ino
+++ src/tinywatch/tinywatch.ino
@@ -1,42 +1,63 @@
+/*
+  tinywatch
+
+  Controls TinyScreen+, BLE Nordic Tinyshield, Proto Board Tinyshield, Li-po Battery 3.7V 270mAh, and DC3V/0.1A 1.5V/0.05A 10x2.7mm Coin Mobile Phone Vibration Motor to work as a smart watch.
+
+  The circuit:
+    * The Vibration Motor or Piezo Element is attached to output pins 5 and 6.
+
+*/
+
 #include <tinywatch-time.h>
 #include <tinywatch-display.h>
 #include <tinywatch-notification.h>
 #include <tinywatch-sleep.h>
 
+//Width of tinyscreen+.
 const int SCREEN_WIDTH = 96;
-int SCREEN_CENTER = SCREEN_WIDTH / 2;
-int piezoPin0 = 5;
-int piezoPin1 = 6;
-
+//Center X coordinate of tinyscreen+.
+const int SCREEN_CENTER = SCREEN_WIDTH / 2;
+int piezoPins = { 5, 6 };
+//Notification flag. True if notification is rendered.
 extern int isNotification;
 
+//True if tinywatch-time or tinywatch-notification is included.
 #if defined(_TINYWATCH_TIME_H_) || defined(_TINYWATCH_NOTIFICATION_H_)
+
+//Derective that enables BLE.
 #define _BLE_ENABLED_
+
+//SPI chip select for the nRF8001.
 const unsigned char BLE_REQ = 10;
+//Interrupt output from the nRF8001 to the tinyscreen+.
 const unsigned char BLE_RDY = 2;
+//Reset signal to the nRF8001.
 const unsigned char BLE_RST = 9;
 
-//Instantiate BLE peripheral.
+//BLE device and pin configuration.
 BLEPeripheral bLEPeripheral = BLEPeripheral(BLE_REQ, BLE_RDY, BLE_RST);
+//BLE device's service with uuid.
+//TODO: Generate uuid.
 BLEService tinywatchService = BLEService("CCC0");
 #endif
 
 void setup() {
-  //Set piezoPin0 and piezoPin1 to OUTPUT to send current to piezo.
-  pinMode(piezoPin0, OUTPUT);
-  pinMode(piezoPin1, OUTPUT);
+  //Set piezoPins[0] and piezoPins[1] to OUTPUT to send current to piezo element.
+  pinMode(piezoPins[0], OUTPUT);
+  pinMode(piezoPins[1], OUTPUT);
+
   //START display plugin.
   TinyWatchDisplay::setup();
   //END display plugin.
 
 #ifdef _BLE_ENABLED_
+  //Name of BLE device when pairing with host device.
   bLEPeripheral.setLocalName("tinywatch");
   //Setting advertising id from service.
   bLEPeripheral.setAdvertisedServiceUuid(tinywatchService.uuid());
   //Adding attributes for BLE peripheral.
   bLEPeripheral.addAttribute(tinywatchService);
-  //Start BLE service.
-
+  
   //START time plugin.
   TinyWatchTime::setup(bLEPeripheral);
   //END time plugin.
@@ -53,6 +74,7 @@ void loop() {
 #ifdef _BLE_ENABLED_
   bLEPeripheral.poll();
 
+  //Do not draw time and date when notification is rendered.
   if(!isNotification){
     //START time plugin.
     TinyWatchTime::drawTime(TinyWatchDisplay::getDisplay());
