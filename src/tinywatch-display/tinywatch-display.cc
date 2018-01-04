5b8f9819d620cb52b9b7d01b7654354bd06247f2
cbolgiano <cbolgiano625@gmail.com>
2018-01-03 21:45:48 -0500

    TW-86: Used cpplint to find/fix style errors for tinywatch-display plugin.

--- src/tinywatch-display/tinywatch-display.cc
+++ src/tinywatch-display/tinywatch-display.cc
@@ -1,23 +1,47 @@
-#include "tinywatch-display.h"
+/*
+MIT License
+
+Copyright (c) 2018 Christopher Bolgiano
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "tinywatch-display/tinywatch-display.h"
 
 extern int isNotification;
 extern int isSleeping;
 
 TinyScreen display = TinyScreen(TinyScreenPlus);
 
-void TinyWatchDisplay::setup(){
+void TinyWatchDisplay::setup() {
   display.begin();
   display.setFlip(true);
   display.setBrightness(10);
 }
 
-void TinyWatchDisplay::manageDisplay(){
-  if(isNotification){
+void TinyWatchDisplay::manageDisplay() {
+  if (isNotification) {
     display.on();
     return;
   }
 
-  if(!isSleeping){
+  if (!isSleeping) {
     display.on();
     return;
   }
@@ -25,6 +49,6 @@ void TinyWatchDisplay::manageDisplay(){
   display.off();
 }
 
-TinyScreen TinyWatchDisplay::getDisplay(){
+TinyScreen TinyWatchDisplay::getDisplay() {
   return display;
 }
