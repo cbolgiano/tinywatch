e9a5302d3fb8057857a865a3a68e51cdf1f7afa3
cbolgiano <cbolgiano625@gmail.com>
2018-01-03 21:59:41 -0500

    TW-86: Used cpplint to find/fix style errors for tinywatch-notification plugin.

--- src/tinywatch-notification/tinywatch-notification.cc
+++ src/tinywatch-notification/tinywatch-notification.cc
@@ -1,4 +1,28 @@
-#include "tinywatch-notification.h"
+/*
+MIT License
+
+Copyright (c) 2018 Christopher Bolgiano
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "tinywatch-notification/tinywatch-notification.h"
 
 extern int SCREEN_CENTER;
 extern int piezoPin0;
@@ -10,36 +34,41 @@ const int TIME_TO_VIBRATE = 2;
 char* msg = "";
 int isNotification = 0;
 
-//notifySleepTime is in seconds.
+// Is in seconds.
 int notifySleepTime = 0;
-//vibrateSleepTime is in seconds.
+// Is in seconds.
 int vibrateSleepTime = 0;
 
-//Notification Characteristic
-BLECharCharacteristic notificationCharacteristic = BLECharCharacteristic("CCC2", BLEWrite);
+// Notification Characteristic
+BLECharCharacteristic notificationCharacteristic =
+  BLECharCharacteristic("CCC2", BLEWrite);
 
-//Setup for notification plugin.
-void TinyWatchNotification::setup(BLEPeripheral& existingPeripheral){
+// Setup for notification plugin.
+void TinyWatchNotification::setup(BLEPeripheral& existingPeripheral) {
   existingPeripheral.addAttribute(notificationCharacteristic);
 
-  notificationCharacteristic.setEventHandler(BLEWritten, setNotificationHandler);
+  notificationCharacteristic.setEventHandler(BLEWritten
+    , setNotificationHandler);
 }
 
-void TinyWatchNotification::setNotificationHandler(BLECentral& central, BLECharacteristic& characteristic) {
+void TinyWatchNotification::setNotificationHandler(BLECentral& central
+  , BLECharacteristic& characteristic) {
   msg = "Alert!";
 }
 
-//Render notification using display.
+// Render notification using display.
 void TinyWatchNotification::drawNotification(TinyScreen display) {
   int width = display.getPrintWidth(msg);
-    
-  drawNotification(display,(SCREEN_CENTER/2) - width, 16,liberationSans_16ptFontInfo, msg);
+
+  drawNotification(display
+    , (SCREEN_CENTER/2) - width, 16, liberationSans_16ptFontInfo, msg);
 }
 
-//Render notification using display.
-void TinyWatchNotification::drawNotification(TinyScreen display, int x, int y, FONT_INFO fontDescriptor, char* customMsg) {
+// Render notification using display.
+void TinyWatchNotification::drawNotification(TinyScreen display
+  , int x, int y, FONT_INFO fontDescriptor, char* customMsg) {
   resetNotification(display);
-  
+
   if (!isNotification && msg != "") {
     isNotification = 1;
     notifySleepTime = now() + TIME_UNTIL_SLEEP;
@@ -51,22 +80,22 @@ void TinyWatchNotification::drawNotification(TinyScreen display, int x, int y, F
   }
 }
 
-//Vibrate piezo element for vibrateSleepTime in seconds.
-void TinyWatchNotification::vibrate() { 
-  if(isNotification && now() <= vibrateSleepTime){
-    //Turn piezo on.
+// Vibrate piezo element for vibrateSleepTime in seconds.
+void TinyWatchNotification::vibrate() {
+  if (isNotification && now() <= vibrateSleepTime) {
+    // Turn piezo on.
     digitalWrite(piezoPin0, HIGH);
     digitalWrite(piezoPin1, HIGH);
   } else {
-    //Turn piezo off.
+    // Turn piezo off.
     digitalWrite(piezoPin0, LOW);
     digitalWrite(piezoPin1, LOW);
-    //Reset vibrate time.
+    // Reset vibrate time.
     vibrateSleepTime = 0;
   }
 }
 
-//START - Helper functions
+// START - Helper functions
 
 void TinyWatchNotification::resetNotification(TinyScreen display) {
   if (isNotification && ((display.getButtons(TSButtonUpperLeft)
@@ -76,9 +105,9 @@ void TinyWatchNotification::resetNotification(TinyScreen display) {
     || now() >= notifySleepTime)) {
     isNotification = 0;
     msg = "";
-    //Reset sleep time.
+    // Reset sleep time.
     notifySleepTime = 0;
   }
 }
 
-//END - Helper functions
+// END - Helper functions
--- src/tinywatch-notification/tinywatch-notification.h
+++ src/tinywatch-notification/tinywatch-notification.h
@@ -1,4 +1,28 @@
-#ifndef _TINYWATCH_NOTIFICATION_H_
+/*
+MIT License
+
+Copyright (c) 2018 Christopher Bolgiano
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef _TINYWATCH_NOTIFICATION_H_ //NOLINT
 #define _TINYWATCH_NOTIFICATION_H_
 
 #ifndef _ARDUINO_H_
@@ -17,24 +41,26 @@
 #include <BLEPeripheral.h>
 #endif
 
-namespace TinyWatchNotification{  
-  //Setup for notifications plugin.
-  void setup(BLEPeripheral& existingPeripheral);
+namespace TinyWatchNotification {
+  // Setup for notifications plugin.
+  void setup(const BLEPeripheral& existingPeripheral);
 
-  //Capture notification.
-  void setNotificationHandler(BLECentral& central, BLECharacteristic& characteristic);
+  // Capture notification.
+  void setNotificationHandler(BLECentral& central
+    , const BLECharacteristic& characteristic);
 
-  //Render notification using display.
+  // Render notification using display.
   void drawNotification(TinyScreen display);
 
-  //Render notification using display with custom parameters.
-  void drawNotification(TinyScreen display, int x, int y, FONT_INFO fontDescriptor, char* customMsg);
+  // Render notification using display with custom parameters.
+  void drawNotification(TinyScreen display
+    , int x, int y, FONT_INFO fontDescriptor, char* customMsg);
 
-  //Vibrate on notifications
+  // Vibrate on notifications
   void vibrate();
 
-  //Reset notification.
+  // Reset notification.
   void resetNotification(TinyScreen display);
-}
+}  // namespace TinyWatchNotification
 
-#endif
+#endif  // _TINYWATCH_NOTIFICATION_H_ NOLINT
